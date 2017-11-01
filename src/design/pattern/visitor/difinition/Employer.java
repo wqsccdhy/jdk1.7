1bea8c51914a0705aca7266848a2b1d568dfc0d2
wqsccdhy <32351704+wqsccdhy@users.noreply.github.com>
2017-11-01 19:45:39 +0800

    访问者模式抽象类提交
    访问者模式抽象类提交

---
+++ src/design/pattern/visitor/difinition/Employer.java
@@ -0,0 +1,37 @@
+package design.pattern.visitor.difinition;
+
+public abstract class Employer {
+	
+	private String name;
+	
+	private int sex;
+	
+	private int salarey;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public int getSalarey() {
+		return salarey;
+	}
+
+	public void setSalarey(int salarey) {
+		this.salarey = salarey;
+	}
+	
+	public abstract void accept();
+
+}
